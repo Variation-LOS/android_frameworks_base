08636e1abc482a5145bce3144ba5ba7654e26208
LuK1337 <priv.luk@gmail.com>
2024-12-15 23:57:17 +0200

    fixup! fw/b: Add support for runtime toggle of navbar
    Change-Id: I8b7b79e979898c826c4d5e7eb424c55a3f9fe0da

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2527,6 +2527,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mGlobalActionsFactory = injector.getGlobalActionsFactory();
         mLockPatternUtils = new LockPatternUtils(mContext);
         mLogger = new MetricsLogger();
+        mLineageHardware = LineageHardwareManager.getInstance(mContext);
 
         Resources res = mContext.getResources();
         mWakeOnDpadKeyPress =
@@ -7363,7 +7364,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
         }
 
-        mLineageHardware = LineageHardwareManager.getInstance(mContext);
         // Ensure observe happens in systemReady() since we need
         // LineageHardwareService to be up and running
         mSettingsObserver.observe();
